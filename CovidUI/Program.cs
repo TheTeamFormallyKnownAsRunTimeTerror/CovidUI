67bec91147ca61c772a4ae23d4cb93983e83b34f
sandorbari <bari.sandor@gmail.com>
2020-06-28 23:24:32 +0100

    introducing Blazorize, progress on displaying historic data

--- CovidUI/Program.cs
+++ CovidUI/Program.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
 
 namespace CovidUI
 {
@@ -15,6 +18,11 @@ namespace CovidUI
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+            builder.Services.AddBlazorise(options => options.ChangeTextOnKeyPress = true)
+                .AddBootstrapProviders()
+                .AddFontAwesomeIcons();
+
             builder.RootComponents.Add<App>("app");
 
             builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
---
+++ CovidUI/Responses/HistoricCountryData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace CovidUI.Responses
+{
+    public class HistoricCountryData
+    {
+        [JsonPropertyName("countryName")]
+        public string CountryName { get; set; }
+        [JsonPropertyName("countryCode")]
+        public string CountryCode { get; set; }
+        [JsonPropertyName("date")]
+        public DateTime Date { get; set; }
+        [JsonPropertyName("active")]
+        public int Active { get; set; }
+        [JsonPropertyName("confirmed")]
+        public int Confirmed { get; set; }
+        [JsonPropertyName("deaths")]
+        public int Deaths { get; set; }
+        [JsonPropertyName("recovered")]
+        public int Recovered { get; set; }
+    }
+}
