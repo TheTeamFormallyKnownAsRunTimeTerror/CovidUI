199bb53e6d06bdd6ce90bf8f8f45a0c2da062319
Bernard Laird <bernard.laird@paddypowerbetfair.com>
2020-07-23 17:26:00 +0100

    Add basics for query builder
    Add docker support
    Add Syncfusion.Blazor

--- CovidUI/Program.cs
+++ CovidUI/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Blazorise;
 using Blazorise.Bootstrap;
 using Blazorise.Icons.FontAwesome;
+using Syncfusion.Blazor;
 
 namespace CovidUI
 {
@@ -22,7 +23,10 @@ namespace CovidUI
             builder.RootComponents.Add<App>("app");
 
             builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            
+            builder.Services.AddSyncfusionBlazor();
+
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MjkwODA2QDMxMzgyZTMyMmUzMGJKcDJCOEJGVm5zTERQVTRsa0ovNndNRWF6UWtmMmtaNWVXbWxTV21udGM9;MjkwODA3QDMxMzgyZTMyMmUzMEtQU1lGWGRiVzVUbGUyZ3dhMGtOSll5a2REbFNBK3ViYzZpaXVxUE40aWM9;MjkwODA4QDMxMzgyZTMyMmUzMEFmajRHenVyemV6cmV2UjRxUDB6Y0lrS0NBWDRaZmJpenkyZXZoZzlRV2M9");
+
             await builder.Build().RunAsync();
         }
     }
